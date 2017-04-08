2b9fa388e47541cf2cfefa9ab8188e2c4c8085ff
Monica <monica.cobzaru@gmail.com>
2017-04-08 16:43:27 -0700

    overloading methods cap5

--- src/Hello.java
+++ src/Hello.java
@@ -3,6 +3,23 @@
  */
 public class Hello {
     public static void main(String[] args) {
+        double cm = calcFeetAndInchesToCentimeters(5,6);
+        System.out.println(cm);
+        System.out.println(calcFeetAndInchesToCentimeters(66));
+    }
+
+    public static double calcFeetAndInchesToCentimeters(double feet, double inches){
+        if(feet >= 0 || inches >=0 || inches <= 12)
+            return 2.54*(feet*12 + inches);
+        else return -1;
+    }
+    public static double calcFeetAndInchesToCentimeters(double inches){
+        if(inches >=0) {
+            //int newInches = inches % 12;
+            //int newFeet = (int) (inches / 12);
+            return calcFeetAndInchesToCentimeters(((int)(inches/12)),inches%12);
+        }
+        else return -1;
 
     }
 
